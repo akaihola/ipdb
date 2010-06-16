b7b0e05e1e28c5952119da4d4040fbf8410ce77a
Alex Kritikos <alex@8bitb.us>
2010-06-17 07:25:45 +0800

    Added pdb's main loop

--- ipdb/__init__.py
+++ ipdb/__init__.py
@@ -18,3 +18,46 @@ def set_trace():
     ip = ipapi.get()
     def_colors = ip.options.colors
     Pdb(def_colors).set_trace(sys._getframe().f_back)
+
+def main():
+    if not sys.argv[1:] or sys.argv[1] in ("--help", "-h"):
+        print "usage: ipdb scriptfile [arg] ..."
+        sys.exit(2)
+
+    mainpyfile =  sys.argv[1]     # Get script filename
+    if not os.path.exists(mainpyfile):
+        print 'Error:', mainpyfile, 'does not exist'
+        sys.exit(1)
+
+    del sys.argv[0]         # Hide "pdb.py" from argument list
+
+    # Replace pdb's dir with script's dir in front of module search path.
+    sys.path[0] = os.path.dirname(mainpyfile)
+
+    # Note on saving/restoring sys.argv: it's a good idea when sys.argv was
+    # modified by the script being debugged. It's a bad idea when it was
+    # changed by the user from the command line. There is a "restart" command which
+    # allows explicit specification of command line arguments.
+
+    # Don't initialize color, since Emacs can't handle it
+    pdb = Pdb()
+    while 1:
+        try:
+            pdb._runscript(mainpyfile)
+            if pdb._user_requested_quit:
+                break
+            print "The program finished and will be restarted"
+        except Restart:
+            print "Restarting", mainpyfile, "with arguments:"
+            print "\t" + " ".join(sys.argv[1:])
+        except SystemExit:
+            # In most cases SystemExit does not warrant a post-mortem session.
+            print "The program exited via sys.exit(). Exit status: ",
+            print sys.exc_info()[1]
+        except:
+            traceback.print_exc()
+            print "Uncaught exception. Entering post mortem debugging"
+            print "Running 'cont' or 'step' will restart the program"
+            t = sys.exc_info()[2]
+            pdb.interaction(None, t)
+            print "Post mortem debugger finished. The "+mainpyfile+" will be restarted"
